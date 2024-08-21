027087c23000b3031fabd19088b8a7be4c51867a
Mikko <realmiks27@gmail.com>
2024-08-21 11:26:53 -0700

    add mobile design of navbar

--- src/app/page.js
+++ src/app/page.js
@@ -6,24 +6,25 @@ import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
     <main>
-      <section className="w-screen h-screen bg-webdev-temp flex flex-col gap-8">
+      {/* <section className="w-screen h-screen bg-webdev-temp flex flex-col gap-8">
         <div className="">
           <Navbar />
         </div>
         <div className="grid grid-cols-2 px-40 justify-between  flex-1">
-          {/* <Image src="/logo.png" alt="logo" 
+          <Image src="/logo.png" alt="logo" 
             width={0}
             height={0}
             sizes="100vw"
             style={{ width: '50%', height: 'auto' }} 
-          /> */}
-          {/* <img src="/logo.png" alt="logo"/> */}
+          />
           <CountDown />
         </div>
         
 
-      </section>
+      </section> */}
       {/* <AboutUs /> */}
+      <Navbar />
+
     </main>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,15 +7,17 @@ import { useState } from "react"
 
 const NavLinks = () => {
     return (
-        <div className="text-black bg-yellowy rounded-4xl hidden md:grid md:grid-cols-6 md:gap-28 md:justify-items-center justify-self-center
+        <div className="grid items-center gap-4 font-semibold justify-items-center text-black md:grid md:grid-cols-6 md:gap-28 md:justify-items-center justify-self-center
         md:items-center md:px-8 md:py-2">
-            <Image src="/logo.png" alt="SF Hacks logo" width={40} height={40}/>
-            <Link href="">Team</Link>
-            <Link href="">FAQs</Link>
+            <Image src="/logo.png" alt="SF Hacks logo" width={40} height={40} className="hidden"/>
+            <Link href="/">Home</Link>
+            <Link href="" className="hidden">Team</Link>
+            <Link href="" >FAQs</Link>
+            <Link href="" >Meet the Officers</Link>
             <Link href="">Sponsors</Link>
-            <Link href="">Alumni</Link>
+            <Link href="">Alumni Wall</Link>
             <div className=""> 
-              <button className="bg-login-button py-1 px-4 rounded-4xl">Log In</button>
+              <button className="hidden ">Log In</button>
             </div>
         </div>
     )
@@ -23,16 +25,21 @@ const NavLinks = () => {
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false)
+    const handleToggle = () => {
+        setIsOpen(!isOpen)
+        console.log('clicked')
+    }
     return (
-        <div className="px-20 py-4 ">
-          <div className="flex justify-end p-2 md:justify-center"> 
-            <div className="flex justify-items-center">
+        <div className="flex justify-end ">
+            <div className={`${isOpen ? 'hidden' : 'block' } px-4 py-2`}>
+                <Menu size={55} onClick={handleToggle} className="text-red-700"/>
+            </div>
+            <div className={`${isOpen ? 'block' : 'hidden' } bg-slate-300 w-5/6 h-screen flex flex-col gap-20`}>
+                <div className="flex justify-end  px-2 ">
+                    <X onClick={handleToggle} size={70} className="text-black"/>
+                </div>
                 <NavLinks />
             </div>
-            <button onClick={() => setIsOpen(!isOpen)} className="md:hidden">
-                {isOpen ? <X /> : <Menu size={50}/>}
-            </button>
-          </div>
         </div>
     )
 }
