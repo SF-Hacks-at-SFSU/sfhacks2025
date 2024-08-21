360cbd519dd687028fd988397fca647427a00201
AkimT13 <akimtarasov@gmail.com>
2024-08-21 15:01:40 -0700

    meet the team photos, discord link, mobile friendlyness, made the countdown a bit more readable, first draft of the website

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 import Image from "next/image";
@@ -7,27 +7,35 @@ import AboutUs from "@/components/aboutUs/AboutUs";
 import Navbar from "@/components/Navbar";
 
 export default function Main() {
-    const [isOpen, setIsOpen] = useState(false)
-    const handleToggle = () => {
-        setIsOpen(!isOpen)
-    }
+  const [isOpen, setIsOpen] = useState(false);
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <div>
-      <section className="w-screen h-screen bg-webdev-temp flex flex-col gap-4 md:gap-12">
+      <section className="w-screen h-screen bg-webdev-temp  flex flex-col gap-4 md:gap-12  ">
         <div className="md:px-24 md:py-4 ">
-          <Navbar isOpen={isOpen} handleToggle={handleToggle}/>
+          <Navbar isOpen={isOpen} handleToggle={handleToggle} />
         </div>
-        <div className="grid justify-items-center">
-          <Image src="/logo.png" alt="logo" 
+        <div className="grid justify-items-center translate-y-16 md:-translate-y-4 scale-[90%] md:scale-100 self-center ">
+          <Image
+            src="/logo.png"
+            alt="logo"
             width={0}
             height={0}
             sizes="100vw"
-            className={`w-1/2 h-auto md:w-1/6 ${isOpen ? 'hidden' : 'block' }`}
+            className={`w-1/2 h-auto md:w-1/2 ${isOpen ? "hidden" : "block"}`}
           />
           <CountDown />
+          <a href="https://discord.gg/P5PsDR6G7W" target="_blank">
+            <button className="bg-sfPink w-40 h-12 text-xl text-black rounded-full">
+              {" "}
+              Discord{" "}
+            </button>
+          </a>
         </div>
       </section>
       <AboutUs />
     </div>
-  )
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,56 +1,76 @@
-"use client"
+"use client";
 
-import Link from "next/link"
-import Image from "next/image"
-import { Menu, X } from "lucide-react"
+import Link from "next/link";
+import Image from "next/image";
+import { Menu, X } from "lucide-react";
 
 const NavLinks = () => {
-    
-    return (
-        <div className="text-2xl grid items-center gap-4 font-semibold justify-items-center text-black 
+  return (
+    <div
+      className="text-2xl grid items-center gap-4 font-semibold justify-items-center text-black 
             md:grid md:grid-cols-6 md:justify-items-center 
             md:items-center md:px-8 md:py-2 md:gap-12 md:text-xl md:bg-sfyellow md:rounded-4xl lg:text-lg lg:gap-20"
+    >
+      <Link href="/" className="hidden md:block">
+        <Image
+          src="/logo.png"
+          alt="logo"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className=" md:h-auto md:w-8 lg:w-10"
+        />
+      </Link>
+      <Link href="/" className="md:hidden">
+        Home
+      </Link>
+      <Link href="" className="hidden md:block md:hover:underline">
+        Team
+      </Link>
+      <Link href="" className="md:hover:underline">
+        FAQs
+      </Link>
+      <Link href="" className="md:hidden">
+        Meet the Officers
+      </Link>
+      <Link href="" className="md:hover:underline">
+        Sponsors
+      </Link>
+      <Link href="" className="md:hover:underline">
+        Alumni <span className="md:hidden">Wall</span>
+      </Link>
+      <div className="">
+        <button
+          className="hidden md:block md:bg-login-button md:px-4 md:py-2 
+              md:rounded-4xl"
         >
-            <Link href="/" className="hidden md:block">
-                <Image src="/logo.png" alt="logo" 
-                    width={0}
-                    height={0}
-                    sizes="100vw"
-                    className=" md:h-auto md:w-8 lg:w-10"
-                />
-            </Link>
-            <Link href="/" className="md:hidden">Home</Link>
-            <Link href="" className="hidden md:block md:hover:underline">Team</Link>
-            <Link href="" className="md:hover:underline">FAQs</Link>
-            <Link href="" className="md:hidden">Meet the Officers</Link>
-            <Link href="" className="md:hover:underline">Sponsors</Link>
-            <Link href="" className="md:hover:underline">Alumni <span className="md:hidden">Wall</span></Link>
-            <div className=""> 
-              <button className="hidden md:block md:bg-login-button md:px-4 md:py-2 
-              md:rounded-4xl">Login</button>
-            </div>
-        </div>
-    )
-}
+          Register
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default function Navbar({isOpen, handleToggle}) {
-
-    return (
-        <div className="flex justify-end md:block">
-            <div className={`${isOpen ? 'hidden' : 'block'} px-4 py-2 md:hidden`}>
-                <Menu size={55} onClick={handleToggle} className="text-black"/>
-            </div>
-            <div className={`${isOpen ? 'block' : 'hidden'} bg-sfyellow w-5/6 h-screen flex 
+export default function Navbar({ isOpen, handleToggle }) {
+  return (
+    <div className="flex justify-end md:block">
+      <div className={`${isOpen ? "hidden" : "block"} px-4 py-2 md:hidden`}>
+        <Menu size={55} onClick={handleToggle} className="text-black" />
+      </div>
+      <div
+        className={`${
+          isOpen ? "block" : "hidden"
+        } bg-sfyellow w-5/6 h-screen flex 
                 flex-col gap-20 md:hidden`}
-            >
-                <div className="flex justify-end px-2 ">
-                    <X onClick={handleToggle} size={70} className="text-black"/>
-                </div>
-                <NavLinks />
-            </div>
-            <div className="hidden  md:flex md:justify-center ">
-                <NavLinks />
-            </div>
+      >
+        <div className="flex justify-end px-2 ">
+          <X onClick={handleToggle} size={70} className="text-black" />
         </div>
-    )
+        <NavLinks />
+      </div>
+      <div className="hidden  md:flex md:justify-center ">
+        <NavLinks />
+      </div>
+    </div>
+  );
 }
