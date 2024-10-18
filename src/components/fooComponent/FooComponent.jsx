1c32b8fe4995ade4a155117b3b3e6aa24a95c07b
ChilRx <chillvireak@gmail.com>
2024-10-18 14:57:24 -0700

    deleted unnecessasry FooComponent

--- src/components/fooComponent/FooComponent.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const FooComponent = () => {
-  return <div className="text-green-500">fooCo n</div>;
-};
-//check protectionss
-//checkint dev branch blahdehbeww
-export default FooComponent;
