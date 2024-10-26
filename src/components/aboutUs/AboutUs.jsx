973af179c54a59fc4a059e6fa8c8552d2a6a68bb
Mikko <realmiks27@gmail.com>
2024-10-25 18:01:05 -0700

    add team on filter buttons clicked

--- src/components/aboutUs/AboutUs.jsx
+++ src/components/aboutUs/AboutUs.jsx
@@ -4,94 +4,132 @@ import React from "react";
 import Person from "./Person";
 import { Button } from "../ui/button";
 import { useState } from "react";
+import {
+  Card, CardContent, CardDescription, 
+  CardFooter, CardHeader, CardTitle, 
+} from "@/components/ui/card"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+import Image from "next/image";
+
 
 const AboutUs = () => {
-  // create the buttons for filter, add state to the clicking of each one, and make it animateable first
-  // then i should create the team array but with team departments first 
-  // and then the people in each department
-  // the departments are (in order): design, engagement, executive, finance, logistics, marketing, outreach, publicity, and tech
-
-  // and then everytime they click a button, the filter state changes
-  // and then set some state to that department and 
-  // then just loop through that state to get the img and name
+ 
   const [filter, setFilter] = useState("Design");
-  const people = [
+  const [people, setPeople] = useState([])
+  const teams = [
     {
       team: "Design",
       members: [
-        {
-          "name": "Gabby",
-          "img": "public/team/gabby.jpg",
-        }
+        { name: "Gabby", img: "/team/gabby.jpg" },
+        { name: "Justine", img: "/team/justine.jpg" },
+        { name: "Louella", img: "/team/louella.jpg" },
+        { name: "Hillary", img: "/team/hillary.jpg" },
+        { name: "Michelle", img: "/team/michelle.png" },
+        { name: "Declan", img: "/team/declan.jpg" },
       ]
     },
     {
       team: "Engagement",
       members: [
+        { name: "Caedyn", img: "/team/caedyn.jpg" },
       ]
     },
     {
       team: "Executive", 
-      members: []
+      members: [
+        { name: "Marco", img: "/team/marco.jpg" },
+        { name: "Ashley", img: "/team/ashley.jpg" },
+        { name: "Ria", img: "/team/ria.jpg" },
+        { name: "Kurt", img: "/team/kurt.jpg" },
+      ]
     },
     {
       team: "Finance",
-      members: []
+      members: [
+        { name: "Vireak", img: "/team/v.JPG" },
+        { name: "Brandon", img: "/team/brandon.jpg" },
+        { name: "Matthew", img: "/team/matthew.jpg" },
+      ]
     },
     {
       team: "Logistics",
-      members: []
+      members: [
+        { name: "Gavin", img: "/team/gavin.jpg" },
+        { name: "Adan", img: "/team/adan.jpg" },
+        { name: "Grishma", img: "/team/grishma.jpg" },
+        { name: "Christian", img: "/team/christian.jpg" },
+        { name: "Stella", img: "/team/stella.jpg" },
+      ]
     },
     {
       team: "Marketing",
-      members: []
+      members: [
+        { name: "John", img: "/team/john.jpg" },
+        // { name: "Trish", img: "/team/trish.jpg" },
+        { name: "William", img: "/team/william.jpg" },
+      ]
     },
     {
       team: "Outreach",
-      members: []
+      members: [
+      ]
     },
     {
       team: "Publicity",
-      members: []
+      members: [
+        { name: "Maxwell", img: "/team/maxwell.jpg" },
+        { name: "Jim", img: "/team/jim.jpg" },
+        { name: "James", img: "/team/james.jpg" },
+      ]
     },
     {
       team: "Tech",
-      members: []
+      members: [
+        { name: "Akim", img: "/team/akim.jpg" },
+        { name: "Avinh", img: "/team/avinh.png" },
+      ]
     }
   ]
+
+  const handleFilter = (team) => {
+    setFilter(team);
+  }
   return (
-    <div className="mt-10 bg-gray-400 grid gap-8 pb-20">
-      <p className="text-center text-7xl text-white">About Us</p>
+    <div className=" bg-sfPink grid gap-8 pb-20">
+      <p className="text-center text-6xl mt-12 md:text-7xl text-black">About Us</p>
       
-      <p className="text-center text-2xl text-white p-6 ">
+      <p className="text-center text-2xl text-black p-6 ">
         SF Hacks is more than just an event, we are a team of passionate
         individuals dedicated to hosting this hackathon. Our diverse team spans
         across logistics, design, tech operations, social engagement, marketing,
         and outreach.
       </p>
-      <p className="text-center text-white text-5xl">Meet Our Team</p>
-      <div className="flex justify-center">
-      <div class="flex max-w-full snap-x snap-mandatory space-x-3 overflow-x-scroll no-scrollbar
-      rounded-full border-2 bg-gray-200 p-2 px-4 ">
-          {people.map((department, i) => (
-            <Button
-            onClick={() => setFilter(department.team)}
-            key={i}
-            variant="outline"
-            className="bg-white inline-block p-2 hover:scale-105 shrink-0 snap-start scroll-ml-4 w-1/6"
-          >
-            {department.team}
-          </Button>
-          ))}
+      <p className="text-center text-black text-5xl">Meet Our Team</p>
+      <div className="grid justify-items-center gap-10 mx-4">
+        <div className="flex max-w-full snap-x snap-mandatory space-x-3 overflow-x-scroll no-scrollbar
+          rounded-full bg-sfDarkPurp gap-4 py-2 px-4 pr-20 ">
+            {teams.map((department, i) => (
+              <Button
+              onClick={() => setFilter(department.team)}
+              key={i}
+              variant="outline"
+              className="bg-white inline-block hover:scale-105 shrink-0 
+              snap-start scroll-ml-4 w-1/2 sm:w-1/4 md:w-1/5 lg:w-1/6 xl:w-1/7"
+            >
+              {department.team}
+            </Button>
+            ))}
         </div>
-        {/* {people.map((person, index) => (
-          <Person
-            name={person.name}
-            position={person.position}
-            img={person.img}
-            key={index}
-          />
-        ))} */}
+        <div className="flex gap-20 flex-wrap">
+          { teams.map((team, i) => {
+            if (team.team === filter) {
+              return team.members.map((person, i) => (
+                <Person name={person.name} img={person.img} key={i} />
+              ));
+            }
+          })}
+        </div>
       </div>
     </div>
   );
--- src/components/aboutUs/Person.jsx
+++ src/components/aboutUs/Person.jsx
@@ -3,17 +3,17 @@ import React from "react";
 
 const Person = ({ name, position, img }) => {
   return (
-    <div className="w-40 h-auto  rounded-2xl ">
+    <div className="w-40 h-auto rounded-2xl ">
       <Image
         src={img}
         width={200}
         height={200}
         alt="Picture of the author"
-        className="aspect-square object-cover rounded-full border-4 border-sfPink object-crop"
+        className="aspect-square object-cover object-center rounded-full border-4 border-sfPink object-crop"
       />
 
-      <p className="text-white text-center text-4xl p-2">{name}</p>
-      <p className="text-white text-center">{position}</p>
+      <p className="text-black text-center text-xl p-2">{name}</p>
+      {/* <p className="text-white text-center">{position}</p> */}
     </div>
   );
 };
