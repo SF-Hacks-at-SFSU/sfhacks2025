80aeb50cf3880e05ccdd5b62f6fc83b1b528f672
ChilRx <chillvireak@gmail.com>
2024-10-30 15:45:35 -0700

    Redid changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const NavLinks = () => {
       <Link href="/" className="md:hidden">
         Home
       </Link>
-      <Link href="" className="md:hover:underline">
+      <Link href="/sponsors" className="md:hover:underline">
         Sponsors
       </Link>
       <Link href="https://tally.so/r/woOWKN" target = '_blank'className="md:hover:underline">
