bfceef05ee2e6b9660da6fd133c344e24f43c507
ChilRx <chillvireak@gmail.com>
2024-11-13 23:48:33 -0800

    Updated site icons

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,17 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
 	title: "SF Hacks 2025",
 	description: "Bigger, Better, with more Air Fryers",
-	icons: [{ rel: "icon", url: "/favicon.ico" }],
+	icons: {
+    // I honestly don't have a good reason to use ico files here but might as well
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/icon.png",
+    other: {
+      rel: "apple-touch-icon-precomposed",
+      url: "/icon.png",
+    },
+  }
+  
 };
 
 export default function RootLayout({ children }) {
