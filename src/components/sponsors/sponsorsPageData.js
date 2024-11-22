f15863bae8a9534a6579bfc2b52c0406db711a51
Huy Dang <ghuydang@gmail.com>
2024-11-22 13:58:33 -0800

    Add tiers

--- src/components/sponsors/sponsorsPageData.js
+++ src/components/sponsors/sponsorsPageData.js
@@ -1,60 +1,106 @@
 export const sponsors = [
-	{
-		name: "Cisco",
-		logoPath: "/sponsor-logos/large-logos/Cisco_logo.png",
-	},
-	{
-		name: "Fastly",
-		logoPath: "/sponsor-logos/large-logos/fastly-ar21.svg",
-	},
-	{
-		name: "Kaiser Permanante",
-		logoPath: "/sponsor-logos/large-logos/Kaiser_Permanente.png",
-	},
-	{
-		name: "Neurelo",
-		logoPath: "/sponsor-logos/large-logos/Neurelo_logo.png",
-	},
-	{
-		name: "Associated Students",
-		logoPath: "/sponsor-logos/AS.png",
-	},
-	{
-		name: "Google Cloud",
-		logoPath: "/sponsor-logos/gcloud.png",
-	},
-	{
-		name: "College of Science and Engineering",
-		logoPath: "/sponsor-logos/medium-logos/COSE_logo.png",
-	},
-	{
-		name: "Innovation Entrepreneurship at San Francisco State University",
-		logoPath: "sponsor-logos/medium-logos/innovation.webp",
-	},
-	{
-		name: "Open AI",
-		logoPath: "/sponsor-logos/large-logos/OpenAI_Logo.svg",
-	},
+  {
+    name: "Cisco",
+    logoPath: "/sponsor-logos/large-logos/Cisco_logo.png",
+    tier: "first",
+  },
+  {
+    name: "Fastly",
+    logoPath: "/sponsor-logos/large-logos/fastly-ar21.svg",
+    tier: "first",
+  },
+  {
+    name: "Kaiser Permanante",
+    logoPath: "/sponsor-logos/large-logos/Kaiser_Permanente.png",
+    tier: "second",
+  },
+  {
+    name: "Neurelo",
+    logoPath: "/sponsor-logos/large-logos/Neurelo_logo.png",
+    tier: "second",
+  },
+  {
+    name: "Associated Students",
+    logoPath: "/sponsor-logos/AS.png",
+    tier: "second",
+  },
+  {
+    name: "Associated Students",
+    logoPath: "/sponsor-logos/AS.png",
+    tier: "second",
+  },
+  {
+    name: "Associated Students",
+    logoPath: "/sponsor-logos/AS.png",
+    tier: "second",
+  },
+  {
+    name: "Associated Students",
+    logoPath: "/sponsor-logos/AS.png",
+    tier: "second",
+  },
+
+  {
+    name: "Google Cloud",
+    logoPath: "/sponsor-logos/gcloud.png",
+    tier: "third",
+  },
+  {
+    name: "Google Cloud",
+    logoPath: "/sponsor-logos/gcloud.png",
+    tier: "third",
+  },
+  {
+    name: "Google Cloud",
+    logoPath: "/sponsor-logos/gcloud.png",
+    tier: "third",
+  },
+  {
+    name: "Google Cloud",
+    logoPath: "/sponsor-logos/gcloud.png",
+    tier: "third",
+  },
+
+  {
+    name: "College of Science and Engineering",
+    logoPath: "/sponsor-logos/medium-logos/COSE_logo.png",
+    tier: "third",
+  },
+  {
+    name: "Innovation Entrepreneurship at San Francisco State University",
+    logoPath: "/sponsor-logos/medium-logos/innovation.webp",
+    tier: "third",
+  },
+  {
+    name: "Open AI",
+    logoPath: "/sponsor-logos/large-logos/OpenAI_Logo.svg",
+    tier: "third",
+  },
+  {
+    name: "Innovation Entrepreneurship at San Francisco State University",
+    logoPath: "/sponsor-logos/medium-logos/innovation.webp",
+    tier: "third",
+  },
 ];
 export const eventImages = [
-	{
-		alt: "Open AI",
-		filePath: "/event-pictures/SF_HACKS_24_O-070.jpg",
-	},
-	{
-		alt: "EcoScan",
-		filePath: "/event-pictures/SF_HACKS_24_O-280.jpg",
-	},
-	{
-		alt: "SOS Hub",
-		filePath: "/event-pictures/SF_HACKS_24_O-281.jpg",
-	},
-	{
-		alt: "Danger Tracker",
-		filePath: "/event-pictures/SF_HACKS_24_O-286.jpg",
-	},
-	{
-		alt: "Bear.Club",
-		filePath: "/event-pictures/SF_HACKS_24_O-397.jpg",
-	},
+  {
+    alt: "Open AI",
+    filePath: "/event-pictures/SF_HACKS_24_O-070.jpg",
+  },
+  {
+    alt: "EcoScan",
+    filePath: "/event-pictures/SF_HACKS_24_O-280.jpg",
+  },
+  {
+    alt: "SOS Hub",
+    filePath: "/event-pictures/SF_HACKS_24_O-281.jpg",
+  },
+  {
+    alt: "Danger Tracker",
+    filePath: "/event-pictures/SF_HACKS_24_O-286.jpg",
+  },
+  {
+    alt: "Bear.Club",
+    filePath: "/event-pictures/SF_HACKS_24_O-397.jpg",
+  },
 ];
