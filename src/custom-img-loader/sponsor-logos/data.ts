643a7eab86834028b499b119c401befe2231d322
Vixeln <vireakcny@gmail.com>
2025-03-01 14:37:56 -0800

    Reordered the sponsors data so that current sponsors are defined first in the array

--- src/custom-img-loader/sponsor-logos/data.ts
+++ src/custom-img-loader/sponsor-logos/data.ts
@@ -19,6 +19,109 @@ export const sponsorTier = {
 
 const data: SponsorsDatum[] = [
 	{
+		name: "Browserbase",
+		path: "/sponsor-logos/current/BrowserBase.png",
+		isCurrent: true,
+
+		tier: "gold",
+		url: "https://www.browserbase.com/"
+	},
+	{
+		name: "SFSU Department of Computer Science",
+		path: "/sponsor-logos/current/SFSU-CSDepartment.svg",
+		isCurrent: true,
+
+		tier: "gold",
+		// This link was grapped directly from their page because I couldn't find it in the Notion document
+		url: "https://cs.sfsu.edu"
+	},
+	{
+		name: "Innovation & Entrepreneurship at San Francisco State University",
+		path: "/sponsor-logos/current/IE.png",
+		isCurrent: true,
+
+		tier: "silver",
+		url: "https://cob.sfsu.edu/initiatives-centers/innovation-entrepreneurship-programs"
+	},
+	{
+		name: "Management Leadership for Tomorrow",
+		path: "/sponsor-logos/current/MLT.png",
+		isCurrent: true,
+
+		tier: "bronze",
+		url: "https://mlt.org/career-prep/"
+	},
+	{
+		name: "Inductive Automation",
+		path: "/sponsor-logos/current/InductiveAutomation.png",
+		isCurrent: true,
+
+		tier: "bronze",
+		// This link was grapped directly from their page because I couldn't find it in the Notion document
+		url: "https://inductiveautomation.com" 
+	},
+	{
+		name: "CodePath",
+		path: "/sponsor-logos/current/CodePath.png",
+		isCurrent: true,
+
+		tier: "bronze",
+		url: "https://www.codepath.org" 
+	},
+	{
+		name: "Fastly",
+		path: "/sponsor-logos/current/Fastly.svg",
+		isCurrent: true,
+
+		tier: "specialThanks", // idk what an a la carte sponsorship level is but imma jsut put bronze
+		url: "https://www.fastly.com/"
+	},
+	{
+		name: "StandOut Stickers",
+		path: "/sponsor-logos/current/stand-out-stickers-logo.png",
+		isCurrent: true,
+
+		tier: "specialThanks",
+		// This link was grapped directly from their page because I couldn't find it in the Notion document
+		url: "http://hackp.ac/mlh-StandOutStickers-hackathons"
+	},
+	{
+		name: "Associated Students",
+		path: "/sponsor-logos/current/AS.png",
+		isCurrent: true,
+
+		tier: "specialThanks",
+		// This link was grapped directly from their page because I couldn't find it in the Notion document
+		url: "https://asi.sfsu.edu"
+	},
+	{
+		name: "Association for Computing Machinery",
+		path: "/sponsor-logos/current/ACM.svg",
+		isCurrent: true,
+
+		tier: "specialThanks",
+		// This link was grapped directly from their page because I couldn't find it in the Notion document
+		url: "https://www.instagram.com/acm.sfsu"
+	},
+	{
+		name: "Lam Family College Of Business at San Francisco State University",
+		path: "/sponsor-logos/current/lamFamily.png",
+		isCurrent: true,
+
+		tier: "specialThanks",
+		// This link was grapped directly from their page because I couldn't find it in the Notion document
+		url: "https://cob.sfsu.edu"
+	},
+	{
+		name: "Dead Eye Coffee Bar",
+		path: "/sponsor-logos/current/DeadEye.png",
+		isCurrent: true,
+
+		tier: "specialThanks",
+		// This link was grapped directly from their page because I couldn't find it in the Notion document
+		url: "https://deadeyecoffeebar.com"
+	},
+	{
 		name: "OpenAI",
 		path: "/sponsor-logos/past/OpenAI_Logo.png",
 		isCurrent: false
@@ -27,14 +130,6 @@ const data: SponsorsDatum[] = [
 		name: "Google Cloud",
 		path: "/sponsor-logos/past/gcloud.png",
 		isCurrent: false
-	},
-	{
-		name: "Browserbase",
-		path: "/sponsor-logos/current/BrowserBase.png",
-		isCurrent: true,
-
-		tier: "gold",
-		url: "https://www.browserbase.com/"
 	},
 	{
 		name: "Zuora",
@@ -52,14 +147,6 @@ const data: SponsorsDatum[] = [
 		isCurrent: false
 	},
 	{
-		name: "Fastly",
-		path: "/sponsor-logos/current/Fastly.svg",
-		isCurrent: true,
-
-		tier: "specialThanks", // idk what an a la carte sponsorship level is but imma jsut put bronze
-		url: "https://www.fastly.com/"
-	},
-	{
 		name: "MongoDB",
 		path: "/sponsor-logos/past/MongoDB_White.svg",
 		isCurrent: false
@@ -68,15 +155,6 @@ const data: SponsorsDatum[] = [
 		name: "ByteDance",
 		path: "/sponsor-logos/past/ByteDance_logo.png",
 		isCurrent: false
-	},
-	{
-		name: "SFSU Department of Computer Science",
-		path: "/sponsor-logos/current/SFSU-CSDepartment.svg",
-		isCurrent: true,
-
-		tier: "gold",
-		// This link was grapped directly from their page because I couldn't find it in the Notion document
-		url: "https://cs.sfsu.edu"
 	},
 	{
 		name: "SFSU College of Science and Engineering",
@@ -107,14 +185,6 @@ const data: SponsorsDatum[] = [
 		name: "Cisco",
 		path: "/sponsor-logos/past/Cisco_logo.png",
 		isCurrent: false
-	},
-	{
-		name: "Management Leadership for Tomorrow",
-		path: "/sponsor-logos/current/MLT.png",
-		isCurrent: true,
-
-		tier: "bronze",
-		url: "https://mlt.org/career-prep/"
 	},
 	{
 		name: "Desmos",
@@ -162,15 +232,6 @@ const data: SponsorsDatum[] = [
 		isCurrent: false
 	},
 	{
-		name: "StandOut Stickers",
-		path: "/sponsor-logos/current/stand-out-stickers-logo.png",
-		isCurrent: true,
-
-		tier: "specialThanks",
-		// This link was grapped directly from their page because I couldn't find it in the Notion document
-		url: "http://hackp.ac/mlh-StandOutStickers-hackathons"
-	},
-	{
 		name: "Sticker Mule",
 		path: "/sponsor-logos/past/Sticker_Mule_logo.svg",
 		isCurrent: false
@@ -195,67 +256,6 @@ const data: SponsorsDatum[] = [
 		path: "/sponsor-logos/past/xyz-logo-color.svg",
 		isCurrent: false
 	},
-	{
-		name: "Associated Students",
-		path: "/sponsor-logos/current/AS.png",
-		isCurrent: true,
-
-		tier: "specialThanks",
-		// This link was grapped directly from their page because I couldn't find it in the Notion document
-		url: "https://asi.sfsu.edu"
-	},
-	{
-		name: "Association for Computing Machinery",
-		path: "/sponsor-logos/current/ACM.svg",
-		isCurrent: true,
-
-		tier: "specialThanks",
-		// This link was grapped directly from their page because I couldn't find it in the Notion document
-		url: "https://www.instagram.com/acm.sfsu"
-	},
-	{
-		name: "Innovation & Entrepreneurship at San Francisco State University",
-		path: "/sponsor-logos/current/IE.png",
-		isCurrent: true,
-
-		tier: "silver",
-		url: "https://cob.sfsu.edu/initiatives-centers/innovation-entrepreneurship-programs"
-	},
-	{
-		name: "Lam Family College Of Business at San Francisco State University",
-		path: "/sponsor-logos/current/lamFamily.png",
-		isCurrent: true,
-
-		tier: "specialThanks",
-		// This link was grapped directly from their page because I couldn't find it in the Notion document
-		url: "https://cob.sfsu.edu"
-	},
-	{
-		name: "Dead Eye Coffee Bar",
-		path: "/sponsor-logos/current/DeadEye.png",
-		isCurrent: true,
-
-		tier: "specialThanks",
-		// This link was grapped directly from their page because I couldn't find it in the Notion document
-		url: "https://deadeyecoffeebar.com"
-	},
-	{
-		name: "Inductive Automation",
-		path: "/sponsor-logos/current/InductiveAutomation.png",
-		isCurrent: true,
-
-		tier: "bronze",
-		// This link was grapped directly from their page because I couldn't find it in the Notion document
-		url: "https://inductiveautomation.com" 
-	},
-	{
-		name: "CodePath",
-		path: "/sponsor-logos/current/CodePath.png",
-		isCurrent: true,
-
-		tier: "bronze",
-		url: "https://www.codepath.org" 
-	}
 ] as const
 
 export default data
