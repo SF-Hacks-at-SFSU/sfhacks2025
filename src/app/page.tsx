8ecc602f9f561fcdd810a35da38a257a46721fc9
Vixeln <vireakcny@gmail.com>
2025-03-08 10:24:05 -0800

    Deleted some unused commented out code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,27 +17,10 @@ export default function Home() {
 							backdropFilter: "blur(4px)",
 							borderRadius: "16px",
 						}}
-
-						// ?????
-						// className={`w-[300px] h-fit ${
-						//     isOpen ? "hidden" : "block"
-						// }`}
 					/>
 				</h2>
 				<CountDown />
-				{/* old discord button */}
-				{/* <button  className="bg-sfPink w-40 h-12 text-xl text-black rounded-full">
-                        {" "}
-                        <a href="https://discord.gg/P5PsDR6G7W" target="_blank">
-                        Discord{" "}
-                        </a>
-                        </button> */}
 			</hgroup>
-			{/* <div className="w-95vw items-center align-contents-center flex flex-col md-gap-6">
-				<div className="mainLandingPage flex flex-col gap-2 w-1/3 h-max items-center ">
-					
-				</div>
-			</div> */}
 		</main>
 	);
 }
