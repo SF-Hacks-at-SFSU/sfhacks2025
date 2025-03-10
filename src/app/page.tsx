806c8a5292ee40bd2698d44435cc8c1426f77709
Vixeln <vireakcny@gmail.com>
2025-03-09 18:59:44 -0700

    Added the following to thesponsors list: Lam Larsen Emergning Tech, TensorStax

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,27 +5,27 @@ import "./styles.css";
 export default function Home() {
 	return (
 		<main id="home">
-				<h2 className="logo">
-					<Image
-						src="/logoNew-horizontal.png"
-						alt="SF Hacks Logo"
-						width={272 * 2}
-						height={92 * 2}
-						style={{
-							backgroundColor: "#fff5d980",
+			<h2 className="logo">
+				<Image
+					src="/logoNew-horizontal.png"
+					alt="SF Hacks Logo"
+					width={272 * 2}
+					height={92 * 2}
+					style={{
+						backgroundColor: "#fff5d980",
 							backdropFilter: "blur(4px)",
-							borderRadius: "16px",
-						}}
-					/>
-				</h2>
-<span className="card">
+						borderRadius: "16px",
+					}}
+				/>
+			</h2>
+			<span className="card">
 				<h3>Theme</h3>
 				<strong>Tech For Good</strong>
 			</span>
 			<div className="card">
 				<h3>Date</h3>
 				<CountDown />
-</div>
+			</div>
 		</main>
 	);
 }
