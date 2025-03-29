b2948a42e7353ef32e01c64974e842e7b689a617
AkimT13 <akimtarasov@gmail.com>
2025-03-28 21:15:09 -0700

    added openmind stuff

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "sfBlue" : "#16133d",
+        "sfPink" : "#16133d",
+        "sponsorBoxBlue" : "#261D4B"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
