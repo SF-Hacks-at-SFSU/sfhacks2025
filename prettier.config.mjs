9be60dd470b3cf54c9f9f19ec8e8178406b86441
Vixeln <vireakcny@gmail.com>
2025-06-21 22:56:31 -0700

    Set Up Pre-commit hook for linting and formatting

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+export default {
+	useTabs: true,
+	tabWidth: 2,
+	singleQuote: true,
+	trailingComma: 'es5',
+	semi: true,
+};
