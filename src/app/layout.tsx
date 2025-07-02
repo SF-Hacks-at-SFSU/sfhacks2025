0f78c6b6951edf9930f40aa5b2d7a828a041e390
Vixeln <vireakcny@gmail.com>
2025-07-01 22:22:46 -0700

    Added MLH 2026 Banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
 
         {/* renamed Huy's bg-webdev-temp to rootBackground */}
         {children}
-				{/* <MLHBanner></MLHBanner> The Banner is hidden until we get the new version for 2026*/}
+				<MLHBanner></MLHBanner>
         <Analytics></Analytics>
       </body>
     </html>
